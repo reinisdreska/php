b1233abfe90b9169114d13d3a2f9839a0fe44ea0
reinisdreska <56246488+reinisdreska@users.noreply.github.com>
2020-11-24 17:34:47 +0200

    Add files via upload

---
+++ tesla/error/not_found.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Lapa nav atrasta!</title>
+</head>
+<body>
+    <div class="col-12">
+        <div>
+            <h1 class="display-3">Error 404</h1>
+            <h2 class="display-4">Lapa nav atrasta!</h2>
+        </div>
+    </div>
+</body>
+</html>
